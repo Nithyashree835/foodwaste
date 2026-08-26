2a99ca4370e35e9dd815621e945f32ffbd39c8bd
Nithyashree <shreenithya206@gmail.com>
2026-08-26 12:15:26 +0530

    Fix contactcontroller annotations

--- src/main/java/com/foodwaste/controller/ContactController.java
+++ src/main/java/com/foodwaste/controller/ContactController.java
@@ -11,8 +11,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/contact")
-@RestController
-@RequestMapping("/api/contact")
 @CrossOrigin(origins = {
         "http://localhost:5173",
         "http://localhost:5174",
